5afcd9a6f5cf3c151eb1cd93dc9f340a4fbaaff7
MarioJim <mario.emilio.j@gmail.com>
2021-02-26 15:18:13 -0600

    Portfolio fetches text from HelloWorldServlet

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -2,3 +2,8 @@ new Splide('#gallery', {
   type: 'loop',
   padding: {right: '5rem', left: '5rem'},
 }).mount();
+
+fetch('/hello').then(response => response.text()).then(text => {
+  const serverP = document.getElementById('server-p');
+  serverP.textContent = text;
+});
