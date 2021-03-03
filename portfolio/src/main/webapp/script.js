fa215b37f00840e94ba0870474d32d0d21804757
MarioJim <mario.emilio.j@gmail.com>
2021-03-02 19:59:43 -0600

    Show random fact below left column

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -3,7 +3,11 @@ new Splide('#gallery', {
   padding: {right: '5rem', left: '5rem'},
 }).mount();
 
-fetch('/hello').then(response => response.text()).then(text => {
-  const serverP = document.getElementById('server-p');
-  serverP.textContent = text;
-});
+const randomFactContainer = document.getElementById('random-fact-container');
+document.getElementById('random-fact-btn').
+    addEventListener('click', async () => {
+      const response = await fetch('/random-fact');
+      const factsList = await response.json();
+      randomFactContainer.textContent = factsList[Math.floor(
+          factsList.length * Math.random())];
+    });
